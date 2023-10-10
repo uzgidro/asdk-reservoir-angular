26bd50f2c35ddd8af6e0704a6c7d8ce0d1099183
suxa56 <62641532+suxa56@users.noreply.github.com>
2023-10-10 13:51:26 +0500

    arrows sort, optimize arrows change

--- src/app/ges-dashboard/ges-dashboard.component.ts
+++ src/app/ges-dashboard/ges-dashboard.component.ts
@@ -11,7 +11,7 @@ export class GesDashboardComponent implements OnInit {
 
   currentTime?: Date
   private selectedTab?: string
-  private ascSort: boolean = true
+  private ascSort?: boolean
 
   constructor(private _timeService: TimeService,
               public _gesService: GesService) {
@@ -22,14 +22,39 @@ export class GesDashboardComponent implements OnInit {
     setInterval(() => this.updateTime(), 1000 * 60 * 20);
   }
 
-  sortByName(event: any) {
+  sortByName(event: any, thead: any) {
+    this.resetArrow(event, thead)
     this.isAscSort(event)
-    this._gesService.sortByName(this.ascSort)
+    this._gesService.sortByName(this.ascSort!!)
   }
 
-  sortByActivePower(event: any) {
+  sortByActivePower(event: any, thead: any) {
+    this.resetArrow(event, thead)
     this.isAscSort(event)
-    this._gesService.sortByPower(this.ascSort)
+    this._gesService.sortByPower(this.ascSort!!)
+  }
+
+  changeArrow(element: any) {
+    if (this.ascSort) {
+      element.data = '../../assets/icons/arrow_up.svg'
+    } else {
+      element.data = '../../assets/icons/arrow_down.svg'
+    }
+  }
+
+  private resetArrow(event: any, thead: any) {
+    if (this.selectedTab != event.target.firstChild.textContent) {
+      // iterate children <th>
+      for (const child of thead.children) {
+        // check if sortable (cursor-pointer)
+        if (child.classList.contains('cursor-pointer')) {
+          // if svg is not 'arrows' -> set 'arrows'
+          if (!child.lastChild.lastChild.data.includes('arrows.svg')) {
+            child.lastChild.lastChild.data = '../../assets/icons/arrows.svg'
+          }
+        }
+      }
+    }
   }
 
   private isAscSort(event: any) {
