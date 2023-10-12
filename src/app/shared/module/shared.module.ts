fac7694e924daea243fd01ade326749fad2d1773
suxa56 <62641532+suxa56@users.noreply.github.com>
2023-10-12 18:02:12 +0500

    republic values on top, refactor

--- src/app/shared/module/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {LoaderComponent} from "../component/loader/loader.component";
-
-
-@NgModule({
-  declarations: [LoaderComponent],
-  exports: [LoaderComponent]
-})
-export class SharedModule { }
