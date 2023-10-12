b750a69bcc624c13913a262112ce39c285189715
suxa56 <62641532+suxa56@users.noreply.github.com>
2023-10-12 11:37:09 +0500

    move time to navbar, fix ges dashboard table rounded,

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TimeService} from "../../service/time.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  currentTime?: Date
+  constructor(public _timeService: TimeService) {
+  }
 
+  ngOnInit() {
+    this.updateTime();
+    setInterval(() => this.updateTime(), 1000);
+  }
+
+  private updateTime() {
+    this._timeService.getCurrentTime().subscribe((data: any) => {
+      this.currentTime = data.datetime;
+    });
+  }
 }
