95f146909358c2861f3709f5cf6ee1bbb81b0eac
suxa56 <62641532+suxa56@users.noreply.github.com>
2023-11-24 10:08:55 +0500

    work with time

--- src/app/reservoir/reservoir.component.ts
+++ src/app/reservoir/reservoir.component.ts
@@ -6,18 +6,43 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./reservoir.component.css']
 })
 export class ReservoirComponent implements OnInit {
-  times = {
-    1: '00:00',
-  2: '06:00',
-  3: '12:00',
-  4: '18:00',
-  }
+
+  times: Date[] = []
 
   async ngOnInit() {
     this.setInfoTime()
   }
 
   private setInfoTime() {
-    console.log(new Date().getHours())
+    const currentTime = new Date().getHours();
+    let currentMonth = new Date().getMonth()
+    let currentDate = new Date().getDate()
+    let currentYear = new Date().getFullYear()
+
+    let roundedTime: number;
+
+    if (currentTime >= 0 && currentTime < 6) {
+      roundedTime = 0;
+    } else if (currentTime >= 6 && currentTime < 12) {
+      roundedTime = 6;
+    } else if (currentTime >= 12 && currentTime < 18) {
+      roundedTime = 12;
+    } else {
+      roundedTime = 18;
+    }
+
+    for (let i = 0; i <= 5; i++) {
+      this.times.push(new Date(currentYear, currentMonth, currentDate, roundedTime))
+      roundedTime -= 6
+      if (roundedTime < 0) {
+        roundedTime = 18
+        if (currentDate == 1) {
+          currentDate -= 1
+          currentMonth -= 1
+        } else {
+          currentDate -= 1
+        }
+      }
+    }
   }
 }
