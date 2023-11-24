530fe287d8d78742944ca97e85d2ee8114196b70
suxa56 <62641532+suxa56@users.noreply.github.com>
2023-11-24 16:21:31 +0500

    fill reservoir table

--- src/app/reservoir/reservoir.component.ts
+++ src/app/reservoir/reservoir.component.ts
@@ -8,6 +8,57 @@ import {Component, OnInit} from '@angular/core';
 export class ReservoirComponent implements OnInit {
 
   times: Date[] = []
+  reservoirs = [
+    {
+      name: 'Андижан',
+      weather: ['Ясно', 'Облачно'],
+      level: [885.32, 885.33],
+      volume: [944.6, 944.9],
+      income: [65, 76, 75, 80, 80, 76],
+      release: 80
+    },
+    {
+      name: 'Охангаран',
+      weather: ['Ясно', 'Ясно'],
+      level: [1043.16, 1043.22],
+      volume: [74, 74.2],
+      income: [1.7, 3.1, 4.4, 4.4, 4.0, 2.8],
+      release: 10
+    },
+    {
+      name: 'Сардоба',
+      weather: ['Облачно', 'Облачно'],
+      level: [885.32, 885.33],
+      volume: [281.20, 281.20],
+      income: [0, 0, 0, 0, 0, 0],
+      release: 30
+    },
+    {
+      name: 'Хисорак',
+      weather: ['Осадки', 'Облачно'],
+      level: [1077.95, 1078.01],
+      volume: [53.1, 53.2],
+      income: [5.3, 4.5, 4.4, 4.5, 5.0, 5.0],
+      release: 10
+    },
+    {
+      name: 'Тупаланг',
+      weather: ['Осадки', 'Осадки'],
+      level: [925.67, 925.73],
+      volume: [263.2, 263.6],
+      income: [11.2, 11.1, 11.1, 11.1, 11.2, 11.2],
+      release: 20
+    },
+    {
+      name: 'Чарвак',
+      weather: ['Ясно', 'Осадки'],
+      level: [877.04, 877.10],
+      volume: [1519.3, 1521],
+      income: [96,96,96,100,95,95],
+      release: 224
+    },
+
+  ]
 
   async ngOnInit() {
     this.setInfoTime()
