ae6a6a564dab1ccdb83265e6e4da60aeecb937c4
suxa56 <suxa5637@mail.ru>
2024-01-08 18:20:40 +0500

    modsnow upload new data

--- src/app/admin/modsnow/modsnow.component.ts
+++ src/app/admin/modsnow/modsnow.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {CalendarModule} from "primeng/calendar";
+import {FormsModule} from "@angular/forms";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-modsnow',
   standalone: true,
-  imports: [],
+  imports: [
+    CalendarModule,
+    FormsModule
+  ],
   templateUrl: './modsnow.component.html',
   styleUrl: './modsnow.component.css'
 })
 export class ModsnowComponent {
+  date = new Date()
+  readonly today = new Date()
+  files: Record<string, File> = {}
 
+  constructor(private messageService: MessageService) {
+  }
+
+  submitModsnow() {
+    this.messageService.add({ severity: 'success', summary: 'Обновление', detail: 'Данные Modsnow обновлены'})
+  }
+
+  documentOnChange(event: any, category: string) {
+    const file:File = event.target.files[0];
+
+    if (file) {
+      this.files[category] = file
+    }
+  }
 }
