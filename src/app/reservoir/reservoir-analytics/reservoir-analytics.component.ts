035220e6230a4faecc2ed024b39e7cf7cfda6526
suxa56 <suxa5637@mail.ru>
2024-01-22 11:01:25 +0500

    analytics page refactor

--- src/app/reservoir/reservoir-analytics/reservoir-analytics.component.ts
+++ src/app/reservoir/reservoir-analytics/reservoir-analytics.component.ts
@@ -1,9 +1,13 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Chart, ChartConfiguration, registerables} from "chart.js";
 import {BaseChartDirective, NgChartsModule} from "ng2-charts";
-import {DecimalPipe, NgForOf} from "@angular/common";
+import {DecimalPipe, NgForOf, NgIf} from "@angular/common";
 import {CalendarModule} from "primeng/calendar";
 import {FormsModule} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {EnvService} from "../../shared/service/env.service";
+import {ReservoirService} from "../reservoir.service";
+import {RegionInfo} from "../../../environments/environment.development";
 
 @Component({
   selector: 'app-reservoir-analytics',
@@ -13,14 +17,17 @@ import {FormsModule} from "@angular/forms";
     NgForOf,
     DecimalPipe,
     CalendarModule,
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './reservoir-analytics.component.html',
   styleUrl: './reservoir-analytics.component.css'
 })
 export class ReservoirAnalyticsComponent implements OnInit {
 
-  private readonly years: YearValue[] = [
+  reservoir?: RegionInfo
+
+  protected readonly years: YearValue[] = [
     {year: 1993, value: 2346},
     {year: 1994, value: 2233},
     {year: 1995, value: 1575},
@@ -57,13 +64,26 @@ export class ReservoirAnalyticsComponent implements OnInit {
   private readonly avgCoefficient = [1.9, 2.1, 5.4, 13.7, 21.3, 22.2, 15.4, 7.5, 3.7, 2.5, 2.3, 2]
   private readonly minCoefficient = [3, 2.6, 5.1, 12.4, 25.8, 23, 11.9, 6.3, 3.9, 2.5, 1.9, 1.9]
   private readonly maxCoefficient = [1.2, 2, 2.9, 16.2, 18.8, 19.6, 19.8, 9.1, 4.2, 2.9, 2.2, 1]
-  private readonly pastYearCoefficient = [1.4,1.5,8.1,18.3,23.3,18.8,13.4,5.2,3.3,2.1,3.1,1.6]
+  private readonly pastYearCoefficient = [1.4, 1.5, 8.1, 18.3, 23.3, 18.8, 13.4, 5.2, 3.3, 2.1, 3.1, 1.6]
+  private readonly randomCoefficient: { min: number, max: number }[] = [
+    {min: 1.2, max: 3},
+    {min: 1.5, max: 2.6},
+    {min: 2.9, max: 8.1},
+    {min: 13.7, max: 18.3},
+    {min: 18.8, max: 25.8},
+    {min: 18.8, max: 23},
+    {min: 11.9, max: 19.8},
+    {min: 5.2, max: 9.1},
+    {min: 3.3, max: 4.2},
+    {min: 2.1, max: 2.9},
+    {min: 1.9, max: 3.1},
+    {min: 1, max: 2}
+  ]
 
-  filteredYears: { year: number, value: number }[] = []
+  chunkedYears: any
+
   startYear = new Date(1993, 0)
-  minDate = new Date(1993, 0)
   endYear = new Date(2023, 0)
-  maxDate = new Date(2023, 0)
   pastYear?: YearValue
   avgValue?: number
   minValue?: YearValue
@@ -72,6 +92,8 @@ export class ReservoirAnalyticsComponent implements OnInit {
   minByMonth: number[] = []
   maxByMonth: number[] = []
   pastYearByMonth: number[] = []
+  selectedYear?: YearValue
+  selectedYearValue?: number[] = []
 
   public lineChartData?: ChartConfiguration['data']
 
@@ -102,23 +124,54 @@ export class ReservoirAnalyticsComponent implements OnInit {
 
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute, private env: EnvService, private resService: ReservoirService) {
     Chart.register(...registerables);
   }
 
   ngOnInit() {
     this.configureData()
     this.getPastYearByMonth()
-    this.pastYear = this.years[this.years.length-1]
+    this.pastYear = this.years[this.years.length - 1]
     this.setupChart()
+    this.activatedRoute.queryParams.subscribe({
+      next: value => {
+        this.reservoir = this.resService.setReservoir(value, this.env.getRegions())
+        this.getAvg()
+      }
+    })
+
+    const chunkValue = 16
+    const result = [];
+    for (let i = 0; i < this.years.length; i += chunkValue) {
+      result.push(this.years.slice(i, i + chunkValue));
+    }
+    this.chunkedYears = result
   }
 
-  dateChange() {
-    this.configureData()
-    this.setupChart()
+  yearSelect(yearValue: YearValue) {
+    let data: number[] = []
+    for (let coef of this.randomCoefficient) {
+      const value = yearValue.value * Math.floor(Math.random() * (coef.max - coef.min) + coef.min) / 100
+      data.push(value)
+    }
+    if (this.selectedYear) {
+      this.lineChartData?.datasets.pop()
+    }
+    this.selectedYearValue = data
+    this.selectedYear = yearValue
+    this.lineChartData?.datasets.push({
+      data: data,
+      label: `${yearValue.year}`,
+      borderColor: 'rgba(147, 51, 234,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderColor: 'rgba(147, 51, 234,1)',
+      pointHoverBackgroundColor: 'rgba(147, 51, 234,0.8)',
+      pointHoverBorderColor: '#fff',
+    })
+    this.chart?.update()
   }
+
   private configureData() {
-    this.filter()
     this.getAvg()
     this.getMax()
     this.getMin()
@@ -127,21 +180,18 @@ export class ReservoirAnalyticsComponent implements OnInit {
     this.getMaxByMonth()
   }
 
-  private filter() {
-    this.filteredYears = this.years.filter(item => item.year >= this.startYear.getFullYear() && item.year <= this.endYear.getFullYear())
-  }
 
   private getAvg() {
-    const sumOfValues = this.filteredYears.reduce((sum, obj) => sum + obj.value, 0);
-    this.avgValue = sumOfValues / this.filteredYears.length;
+    const sumOfValues = this.years.reduce((sum, obj) => sum + obj.value, 0);
+    this.avgValue = sumOfValues / this.years.length;
   }
 
   private getMin() {
-    this.minValue = this.filteredYears.reduce((min, obj) => (obj.value < min.value ? obj : min), this.filteredYears[0]);
+    this.minValue = this.years.reduce((min, obj) => (obj.value < min.value ? obj : min), this.years[0]);
   }
 
   private getMax() {
-    this.maxValue = this.filteredYears.reduce((max, obj) => (obj.value > max.value ? obj : max), this.filteredYears[0]);
+    this.maxValue = this.years.reduce((max, obj) => (obj.value > max.value ? obj : max), this.years[0]);
   }
 
   private getAvgByMonth() {
@@ -168,7 +218,7 @@ export class ReservoirAnalyticsComponent implements OnInit {
   private getPastYearByMonth() {
     if (this.endYear)
       for (let i = 0; i < 12; i++) {
-        this.pastYearByMonth[i] = this.years[this.years.length-1].value * this.pastYearCoefficient[i] / 100
+        this.pastYearByMonth[i] = this.years[this.years.length - 1].value * this.pastYearCoefficient[i] / 100
       }
   }
 
@@ -178,46 +228,38 @@ export class ReservoirAnalyticsComponent implements OnInit {
         {
           data: this.minByMonth,
           label: `Минимум ${this.minValue?.year}`,
-          backgroundColor: 'rgba(22, 163, 74,0.2)',
-          borderColor: 'rgba(22, 163, 74,1)',
-          pointBackgroundColor: 'rgba(22, 163, 74,1)',
-          pointBorderColor: '#fff',
-          pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgba(22, 163, 74,0.8)',
-          fill: 'origin',
+          borderColor: 'rgba(225, 29, 72,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderColor: 'rgba(225, 29, 72,1)',
+          pointHoverBackgroundColor: 'rgba(225, 29, 72,0.8)',
+          pointHoverBorderColor: '#fff',
         },
         {
           data: this.avgByMonth,
           label: `Среднее за года (${this.startYear.getFullYear()} - ${this.endYear.getFullYear()})`,
-          backgroundColor: 'rgba(37, 99, 235,0.2)',
           borderColor: 'rgba(37, 99, 235,1)',
-          pointBackgroundColor: 'rgba(37, 99, 235,1)',
-          pointBorderColor: '#fff',
-          pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgba(37, 99, 235,0.2)',
-          fill: 'origin',
+          pointBackgroundColor: '#fff',
+          pointBorderColor: 'rgba(37, 99, 235,1)',
+          pointHoverBackgroundColor: 'rgba(37, 99, 235,0.2)',
+          pointHoverBorderColor: '#fff',
         },
         {
           data: this.pastYearByMonth,
-          label: `Данные за прошлый ${this.years[this.years.length-1].year}`,
-          backgroundColor: 'rgba(225, 29, 72,0.2)',
-          borderColor: 'rgba(225, 29, 72,1)',
-          pointBackgroundColor: 'rgba(225, 29, 72,1)',
-          pointBorderColor: '#fff',
-          pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgba(225, 29, 72,0.8)',
-          fill: 'origin',
+          label: `Данные за прошлый ${this.years[this.years.length - 1].year}`,
+          borderColor: 'rgba(217, 119, 6,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderColor: 'rgba(217, 119, 6,1)',
+          pointHoverBackgroundColor: 'rgba(217, 119, 6,0.2)',
+          pointHoverBorderColor: '#fff',
         },
         {
           data: this.maxByMonth,
           label: `Максимум ${this.maxValue?.year}`,
-          backgroundColor: 'rgba(217, 119, 6,0.2)',
-          borderColor: 'rgba(217, 119, 6,1)',
-          pointBackgroundColor: 'rgba(217, 119, 6,1)',
-          pointBorderColor: '#fff',
-          pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgba(217, 119, 6,0.2)',
-          fill: 'origin',
+          borderColor: 'rgba(22, 163, 74,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderColor: 'rgba(22, 163, 74,1)',
+          pointHoverBackgroundColor: 'rgba(22, 163, 74,0.8)',
+          pointHoverBorderColor: '#fff',
         },
       ],
       labels: ['Янв.', 'Фев.', 'Март', 'Апр.', 'Май', 'Июнь', 'Июль', 'Авн.', 'Сент.', 'Окт.', 'Ноя.', 'Дек'],
