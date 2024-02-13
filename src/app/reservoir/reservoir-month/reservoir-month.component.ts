1c6d345ba016befa6d75fdb138f7ef2bfc115a9c
suxa56 <suxa5637@mail.ru>
2024-02-13 16:11:14 +0500

    monthly values

--- src/app/reservoir/reservoir-month/reservoir-month.component.ts
+++ src/app/reservoir/reservoir-month/reservoir-month.component.ts
@@ -2,8 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ApiService} from "../../service/api.service";
 import {CategorisedValueResponse} from "../../shared/response/values-response";
-import _default from "chart.js/dist/plugins/plugin.tooltip";
-import numbers = _default.defaults.animations.numbers;
+import {ReservoirResponse} from "../../shared/response/reservoir-response";
 
 @Component({
   selector: 'app-reservoir-month',
@@ -11,15 +10,13 @@ import numbers = _default.defaults.animations.numbers;
   styleUrl: './reservoir-month.component.css'
 })
 export class ReservoirMonthComponent implements OnInit {
-  reservoir?: string
-  months: Date[] = []
+  reservoirName?: string
   data: {
     date: Date
-    tenDays: number[]
-    elevenDays: number[]
-    firstDecadeLimit: number
-    secondDecadeLimit: number
-    thirdDecadeLimit: number
+    income?: any[]
+    release?: any[]
+    level?: any[]
+    volume?: any[]
   }[] = []
 
   constructor(private activatedRoute: ActivatedRoute, private api: ApiService) {
@@ -28,79 +25,83 @@ export class ReservoirMonthComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe({
       next: value => {
-        this.api.getMonthReservoirValues(value['reservoir']).subscribe({
-          next: (response: CategorisedValueResponse) => {
-            this.reservoir = response.income.reservoir
-
+        this.api.getReservoirById(value['reservoir']).subscribe({
+          next: (response: ReservoirResponse) => {
+            this.reservoirName = response.name
           }
         })
-        // this.reservoir = this.resService.setReservoir(value, this.env.getRegions())
+        this.api.getMonthReservoirValues(value['reservoir']).subscribe({
+          next: (response: CategorisedValueResponse) => {
+            this.data = []
+            this.setupTableData(response)
+          }
+        })
       }
     })
-    this.setupMonths()
-    this.setupData()
   }
 
-  private setupMonths() {
-    for (let i = 0; i < 12; i++) {
-      this.months.push(new Date(new Date().getFullYear(), i))
-    }
-  }
-
-  getAvg(array: number[], limit: number) {
-    let sum = 0
-    for (let i = 0; i < limit; i++) {
-      sum += array[i]
-    }
-    if (sum / limit) {
-    return sum / limit
+  getAvg(array: any[]) {
+    const numbersArray: number[] = array.filter(item => typeof item === 'number');
+    if (numbersArray.length > 0) {
+      return numbersArray.reduce((sum, num) => sum + num, 0) / numbersArray.length;
     } else {
-      return ''
+      return 0
     }
   }
 
-  private setupData() {
-    const today = new Date()
-
-    let month: Date
-    let tenDays = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    let elevenDays = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-    for (let i = 0; i < 12; i++) {
-      let firstDecadeLimit = 0
-      let secondDecadeLimit = 0
-      let thirdDecadeLimit = 0
-      month = new Date(new Date().getFullYear(), i)
-      if (month.getMonth() < today.getMonth()) {
-        const date = month.setFullYear(new Date().getFullYear(), i+1, 0)
-        firstDecadeLimit = 10
-        secondDecadeLimit = 10
-        thirdDecadeLimit = new Date(date).getDate() - 20
-      } else if (month.getMonth() === today.getMonth()) {
-        if (today.getDate() < 11) {
-          firstDecadeLimit = today.getDate()
-        } else if (today.getDate() < 21) {
-          firstDecadeLimit = 10
-          secondDecadeLimit = today.getDate() - 10
-        } else {
-          firstDecadeLimit = 10
-          secondDecadeLimit = 10
-          thirdDecadeLimit = today.getDate() - 20
+  private setupTableData(response: CategorisedValueResponse) {
+    for (let income of response.income.data) {
+      let valueDate = new Date(income.date)
+      let monthData = this.data.find(value => value.date.getMonth() == valueDate.getMonth())
+      if (monthData && monthData.income) {
+        monthData.income.push(income.value)
+      } else {
+        this.data[valueDate.getMonth()] = {
+          date: valueDate,
+          income: [income.value]
         }
       }
-    this.data.push({
-      date: month!!,
-      tenDays: tenDays,
-      elevenDays: elevenDays,
-      firstDecadeLimit: firstDecadeLimit,
-      secondDecadeLimit: secondDecadeLimit,
-      thirdDecadeLimit: thirdDecadeLimit
-    })
-      if (month.getMonth() === today.getMonth()) {
-        break
+    }
+    for (let release of response.release.data) {
+      let valueDate = new Date(release.date)
+      let monthData = this.data.find(value => value.date.getMonth() == valueDate.getMonth())
+      if (monthData && monthData.release) {
+        monthData.release.push(release.value)
+      } else if (monthData) {
+        monthData.release = [release.value]
       }
     }
-
+    for (let level of response.level.data) {
+      let valueDate = new Date(level.date)
+      let monthData = this.data.find(value => value.date.getMonth() == valueDate.getMonth())
+      if (monthData && monthData.level) {
+        monthData.level.push(level.value)
+      } else if (monthData) {
+        monthData.level = [level.value]
+      }
+    }
+    for (let volume of response.volume.data) {
+      let valueDate = new Date(volume.date)
+      let monthData = this.data.find(value => value.date.getMonth() == valueDate.getMonth())
+      if (monthData && monthData.volume) {
+        monthData.volume.push(volume.value)
+      } else if (monthData) {
+        monthData.volume = [volume.value]
+      }
+    }
+    for (let item of this.data) {
+      if (item.income && item.income?.length < 31) {
+        item.income = [...item.income, ...new Array(31 - item.income.length).fill('')];
+      }
+      if (item.release && item.release?.length < 31) {
+        item.release = [...item.release, ...new Array(31 - item.release.length).fill('')];
+      }
+      if (item.level && item.level?.length < 31) {
+        item.level = [...item.level, ...new Array(31 - item.level.length).fill('')];
+      }
+      if (item.volume && item.volume?.length < 31) {
+        item.volume = [...item.volume, ...new Array(31 - item.volume.length).fill('')];
+      }
+    }
   }
 }
