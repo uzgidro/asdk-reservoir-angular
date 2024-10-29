3ecf350fa9e9fa42c99906f0e866aaa94193e178
suxa56 <suxa5637@mail.ru>
2024-10-29 17:30:05 +0500

    nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGray: '#1d1c1c',
+        deepBlack: '#080e0f',
+        brightBlue: '#01f5f5',
+      },
+    },
   },
   plugins: [],
 }
