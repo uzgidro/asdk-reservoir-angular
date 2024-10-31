4ed83e138c31a861a3211c8b1afc50708c643750
suxa56 <suxa5600@gmail.com>
2024-10-31 23:03:01 +0500

    dashboard work started

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-black': '#060606',
         'dark-gray': '#1d1c1c',
         'deep-black': '#080e0f',
-        'bright-blue': '#01f5f5',
+        'background': '#060606',
+        'primary': '#01f5f5',
+        'primary-container': '#00ffff',
+        'secondary': '#003c54',
+        'on-secondary': '#014a67',
+        'secondary-container': '#066684',
+        'secondary-variant': '#00506f',
+        'secondary-variantus': '#00263b',
+        'tertiary-container': '#023f52',
       },
     },
   },
