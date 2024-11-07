37192ded32639812b2b034b45c5b683925cceb1d
suxa56 <suxa5637@mail.ru>
2024-11-07 12:12:36 +0500

    water recourses page

--- src/app/water-recourses/water-recourses.component.ts
+++ src/app/water-recourses/water-recourses.component.ts
@@ -2,8 +2,9 @@ import {Component} from '@angular/core';
 import {CardHeaderComponent} from "../shared/component/card-header/card-header.component";
 import {NgChartsModule} from "ng2-charts";
 import {RouterLink} from "@angular/router";
-import {ChartConfiguration, ChartData, Plugin} from "chart.js";
+import {ChartConfiguration, ChartType, Plugin} from "chart.js";
 import ChartDataLabels from "chartjs-plugin-datalabels";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-water-recourses',
@@ -11,28 +12,33 @@ import ChartDataLabels from "chartjs-plugin-datalabels";
   imports: [
     CardHeaderComponent,
     NgChartsModule,
-    RouterLink
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './water-recourses.component.html',
   styleUrl: './water-recourses.component.css'
 })
 export class WaterRecoursesComponent {
   public reservoirs = ['Ohangaron', 'Andijon', 'Hisorak', 'To\'palang', 'Chorog', 'Sardoba']
-  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        borderColor: 'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+      }
+    ],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  };
+
+  public lineChartOptions: ChartConfiguration['options'] = {
     aspectRatio: 2.5,
-    // maintainAspectRatio: false,
-    // We use these empty structures as placeholders for dynamic theming.
     scales: {
       x: {
-        grid: {
-          display: false,
-        },
-        ticks: {
-          color: 'white',
-        }
-      },
-      y: {
-        min: 10,
         grid: {
           color: '#2D2D2D',
         },
@@ -40,43 +46,21 @@ export class WaterRecoursesComponent {
           color: 'white',
         }
       },
-
-    },
-    plugins: {
-      tooltip: {
-        enabled: false
-      },
-      legend: {
-        display: true,
-        labels: {
+      y: {
+        grid: {
+          color: '#2D2D2D',
+        },
+        ticks: {
           color: 'white',
         }
       },
-      datalabels: {
-        color: 'white',
-        align: "end",
-        anchor: "end"
-      }
+    },
+
+    plugins: {
+      legend: {display: false},
     },
   };
-  public barChartType = 'bar' as const;
-  public chartPlugin = [ChartDataLabels] as Plugin<'bar'>[];
 
-  public barChartData: ChartData<'bar'> = {
-    labels: this.reservoirs,
-    datasets: [
-      {
-        data: [65, 59, 80, 81, 56, 55],
-        label: '06:00',
-        backgroundColor: '#014a67',
-        barThickness: 36,
-      },
-      {
-        data: [28, 48, 40, 19, 86, 27],
-        label: '12:00',
-        backgroundColor: '#4eeefe',
-        barThickness: 36,
-      },
-    ],
-  };
+  public lineChartType: ChartType = 'line';
+  public chartPlugin = [ChartDataLabels] as Plugin<'bar'>[];
 }
