37b5ed02e997baee0b48f6ba533c9a3379076966
suxa56 <suxa5600@gmail.com>
2025-04-23 09:19:01 +0500

    return vertical current chart

--- src/app/dashboard/dashboard-current-chart/dashboard-current-chart.component.ts
+++ src/app/dashboard/dashboard-current-chart/dashboard-current-chart.component.ts
@@ -39,7 +39,7 @@ export class DashboardCurrentChartComponent
         this.releaseData = this.setupData(response.release)
         this.levelData = this.setupData(response.level)
         this.volumeData = this.setupData(response.volume)
-        this.renderHorizontalCategoryChart(this.incomeData)
+        this.renderVerticalCategoryChart(this.incomeData)
       }
     })
   }
