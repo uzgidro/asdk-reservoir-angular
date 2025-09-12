181a671a737f2af593ae38eaba57771e2d78a63f
suxa56 <suxa5600@gmail.com>
2025-09-12 13:27:01 +0500

    navbar ul text and card-header text adaptive

--- src/app/low-chatkal/l-c-dashboard/l-c-dashboard.component.ts
+++ src/app/low-chatkal/l-c-dashboard/l-c-dashboard.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CardWrapperComponent} from "../../shared/component/card-wrapper/card-wrapper.component";
+import {CardHeaderComponent} from "../../shared/component/card-header/card-header.component";
 
 @Component({
   selector: 'app-l-c-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CardWrapperComponent,
+    CardHeaderComponent
+  ],
   templateUrl: './l-c-dashboard.component.html',
   styleUrl: './l-c-dashboard.component.css'
 })
