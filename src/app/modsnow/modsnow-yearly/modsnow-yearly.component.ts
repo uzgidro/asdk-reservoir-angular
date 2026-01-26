7549cf2d49d7e09e6e474fffa130be60ed995ecd
suxa56 <suxa5600@gmail.com>
2026-01-26 19:20:18 +0500

    modsnow refactor

--- src/app/modsnow/modsnow-yearly/modsnow-yearly.component.ts
+++ src/app/modsnow/modsnow-yearly/modsnow-yearly.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {CarouselModule} from "primeng/carousel";
-import {NgOptimizedImage} from "@angular/common";
 import {SharedModule} from "primeng/api";
 import {CardHeaderComponent} from "../../shared/component/card-header/card-header.component";
 import {CardWrapperComponent} from "../../shared/component/card-wrapper/card-wrapper.component";
@@ -13,7 +12,6 @@ import {LoaderComponent} from "../../shared/component/loader/loader.component";
   selector: 'app-modsnow-yearly',
   imports: [
     CarouselModule,
-    NgOptimizedImage,
     SharedModule,
     CardHeaderComponent,
     CardWrapperComponent,
